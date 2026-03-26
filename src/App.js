d9665c0c1d5367d6e1e8c711b665438920ee2ec7
praveen kumar a x <xav.axp@gmail.com>
2026-03-26 16:48:22 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import { useState } from "react";
 import "./App.css";
 import GameSection from "./Components/GameSection";
+import PreviewSection from "./Components/PreviewSection";
 
 function App() {
   const [gameover, setGameover] = useState(false);
+  const [showPreview, setShowPreview] = useState(true);
 
   return (
-    <div className="App flex">
-      <GameSection gameover={gameover} setGameover={setGameover} />
+    <div className="App flex appRoot">
+      <div className="appModeBar">
+        <button
+          className="modeToggleBtn"
+          onClick={() => setShowPreview((prev) => !prev)}
+        >
+          {showPreview ? "Back To Game" : "Open Preview Section"}
+        </button>
+      </div>
+
+      {showPreview ? (
+        <PreviewSection />
+      ) : (
+        <GameSection gameover={gameover} setGameover={setGameover} />
+      )}
     </div>
   );
 }
