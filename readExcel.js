aa87a3b710790dcdb72576d630f30f1e40b4da0f
praveen kumar a x <xav.axp@gmail.com>
2026-03-26 16:48:12 +0530

    Update readExcel.js

--- readExcel.js
+++ readExcel.js
@@ -1,33 +1,31 @@
-const reader = require('xlsx')
+const reader = require("xlsx");
 // Reading our test file
-const file = reader.readFile('./Book.xlsx')
-const data = []
-const sheets = file.SheetNames
+// const file = reader.readFile('./Book.xlsx')
+const file = reader.readFile("./Book2.csv");
+const data = [];
+const sheets = file.SheetNames;
 for (let i = 0; i < sheets.length; i++) {
-    const temp = reader.utils.sheet_to_json(
-        file.Sheets[file.SheetNames[i]])
-    temp.forEach((res) => {
-        data.push(res)
-    })
+  const temp = reader.utils.sheet_to_json(file.Sheets[file.SheetNames[i]]);
+  temp.forEach((res) => {
+    data.push(res);
+  });
 }
 // Printing data
-console.log(data)
-const output = []
-data.forEach(v => {
-    output.push({
-        'title': v.title,
-        'image': v.image,
-        'time': {
-            'year': v.year,
-            'ad': v.ad,
-            'month': v.month,
-            'day': v.day,
-            'display': v.display,
-            
-        }
-    });
-
-})
+console.log(data);
+const output = [];
+data.forEach((v) => {
+  output.push({
+    title: v.title,
+    image: v.image,
+    time: {
+      year: v.year,
+      ad: v.ad,
+      month: v.month,
+      day: v.day,
+      display: v.display,
+    },
+  });
+});
 // console.log(output)
 var json = JSON.stringify(output);
-console.log(json)
+console.log(json);
