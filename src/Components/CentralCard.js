c20d88f87a77a6a62396c48a8cb14ee194165208
praveen kumar a x <xav.axp@gmail.com>
2026-03-26 16:03:58 +0530

    Update CentralCard.js

--- src/Components/CentralCard.js
+++ src/Components/CentralCard.js
@@ -1,25 +1,23 @@
 import { motion } from "framer-motion"
 
 function CentralCard({ data, onSelect }) {
+    const onImageError = (event) => {
+        event.currentTarget.onerror = null;
+        event.currentTarget.src = "/logo192.png";
+    };
+
     return (
         <motion.div className="centralCard" onClick={() => onSelect()} whileHover={{
             scale: 1.1,
             transition: { duration: 0.5 },
         }}>
-            <img className="centralCardImage" src={data["image"]} alt="" />
+            <img className="centralCardImage" src={data["image"]} alt={data["title"] || "Historical event card"} onError={onImageError} />
             
             
 
           
             <div className="centralCardText">{data["title"]}</div>
 
-            {/* <div id="discardSpace">
-
-            </div>
-
-            <div id="selectSpace">
-
-            </div> */}
         </motion.div>
     )
 }
