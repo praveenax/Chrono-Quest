632910728c66c56e68c8328086dcf3b0dd1ede77
praveen kumar a x <xav.axp@gmail.com>
2026-03-26 16:05:57 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,47 +1,13 @@
 import { useState } from "react";
 import './App.css';
-// import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd';
-// import cards from './cards.json';
-
-
 import GameSection from "./Components/GameSection";
-
-// const allCardList = cards;
-
-//shows menu and level selection
-// function MenuScreen() {
-//   return (
-//     <>
-//       <div>
-//         Chrono Quest!
-//         <div>
-//           <button>New Game</button>
-//           {/* <button>Levels</button> */}
-//           <button>Quit</button>
-//         </div>
-//       </div>
-//     </>
-//   )
-// }
 
 function App() {
   const [gameover, setGameover] = useState(false);
+
   return (
     <div className="App flex">
-      {
-        // !gameover && (
-        <GameSection gameover={gameover} setGameover={setGameover} />
-        // )
-      }
-
-      {/* {
-        gameover && (
-          <>
-            Game Over
-          </>
-        )
-      } */}
-
+      <GameSection gameover={gameover} setGameover={setGameover} />
     </div>
   );
 }
